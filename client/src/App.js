5f8dac9641c6d234e30db6aea91d197c75cda94e
volkanGoksu <volkan.goksu@outlook.com>
2021-06-06 00:52:08 +0300

    signin and signout with firebase

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import 'firebase/auth'
 import {useAuthState} from 'react-firebase-hooks/auth'
 import {useCollectionData} from 'react-firebase-hooks/firestore'
 
+
+
 firebase.initializeApp({
   apiKey: "AIzaSyBCfIxO-aIcMKGDD7Ne8Ga9J6cz13waSJE",
     authDomain: "react-fire-chat-app-5410b.firebaseapp.com",
@@ -18,11 +20,52 @@ const auth = firebase.auth();
 const fireStore = firebase.firestore();
 
 function App() {
+  const [user] = useAuthState(auth)
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <header>
+        <SignOut/>
+      </header>
+      <section>
+        {user ? <ChatRoom/> : <SignIn/>}
+      </section>
     </div>
   );
 }
 
 export default App;
+
+function SignIn(){
+
+  const SignInGoogle=()=>{
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+  return(
+    <div>
+      <button className="sign in" onClick={SignInGoogle}>Giriş Yap</button>
+    </div>
+  )
+}
+
+function SignOut(){
+
+  const SignOutGoogle=()=>{
+    auth.signOut();
+  }
+   return(
+      
+        auth.currentUser && (
+          <button className="sign out" onClick={SignOutGoogle}>
+              Çıkış Yap
+          </button>
+        )
+      
+   )
+}
+
+function ChatRoom(){
+  return(
+    <div></div>
+  )
+}
